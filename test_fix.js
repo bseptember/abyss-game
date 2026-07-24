ca37c0f19b81ee4d9bbb7cebe68b087b94bf0e2e
Oracle Public Cloud User <opc@hermes-agent.publicsubnet.aimusiclabelvcn.oraclevcn.com>
2026-07-24 09:28:37 +0000

    chore: drop Hermes scratch test files from bugfix branch

--- test_fix.js
+++
@@ -1,9 +0,0 @@
-const game = document.querySelector('.test').onclick = () => {
-  localStorage.setItem('abyss-best', '0');
-  localStorage.setItem('abyss-leaderboard', '[]');
-  
-  console.log('Before gameOverShown:', game.gameOverShown);
-  game.die();
-  console.log('After die(), before timeout - gameOverShown:', game.gameOverShown, 'alive:', game.alive);
-  console.log('Game over visible?', !document.getElementById('game-over').classList.contains('hidden'));
-};
